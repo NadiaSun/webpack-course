10c0cf617d4aa69bbe5ae1470faf526fba287bb6
NadiaSun <59049445+NadiaSun@users.noreply.github.com>
2022-07-04 10:43:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './scss/index.scss'
+// import './scss/index.scss'
 import {Router} from '@core/routes/Router';
 import {DashboardPage} from '@/pages/DashboardPage';
 import {ExcelPage} from '@/pages/ExcelPage';
