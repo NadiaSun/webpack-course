aaecdfea66ea8dbc893fb054f594625325db45bb
NadiaSun <59049445+NadiaSun@users.noreply.github.com>
2022-07-04 14:47:52 +0300

    Add files via upload

---
+++ docs/index.js
@@ -0,0 +1,11 @@
+import './scss/index.scss'
+import {Router} from '@core/routes/Router';
+import {DashboardPage} from '@/pages/DashboardPage';
+import {ExcelPage} from '@/pages/ExcelPage';
+import {ErrorPage} from '@/pages/ErrorPage';
+
+new Router('#app', {
+    dashboard: DashboardPage,
+    excel: ExcelPage,
+    error: ErrorPage
+})
