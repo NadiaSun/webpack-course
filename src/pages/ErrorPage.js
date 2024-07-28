21526f2d3bad5e185f51b57677cfa5d859955589
NK <59049445+NadiaSun@users.noreply.github.com>
2024-07-28 15:34:25 +0300

    Update ErrorPage.js
    language update

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -5,12 +5,12 @@ export class ErrorPage extends Page {
         return $.create('div', 'db').html(`
         <div class="error">
             <div class="error__text">
-                 <h2>Произошла ошибка. Неправильный хэш таблицы</h2>
+                 <h2>An error has occurred. Invalid table hash</h2>
                  <img class="img" src="snake.png" alt="">
             </div>
             <h3 class="error__return">
                 <a href="#dashboard">
-                    Вернуться на главную страницу
+                    Go back to main page
                 </a>
             </h3>
         </div>
